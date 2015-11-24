6a5a6055455425709805fce2e97eed5bf7f2216e
Ben Davis <bendavis78@gmail.com>
2015-11-24 14:29:45 -0600

    update

--- freedcamp-calendar-link.user.js
+++ freedcamp-calendar-link.user.js
@@ -12,7 +12,8 @@
 
 var $ = document.querySelector.bind(document);
 
-var $ul = $('.fc-user-actions ul.app');
+var $ul = $('.fc-user-actions > ul');
+console.log($ul);
 
 var $link = document.createElement('link');
 $link.href = 'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css';
@@ -24,10 +25,18 @@ function addButton(icon, name, href) {
   var $icon = document.createElement('i');
   $icon.setAttribute('class', 'fa fa-' + icon);
   $icon.style.color = '#fff';
+    
+  var $span = document.createElement('span');
+  $span.appendChild($icon);
+  $span.setAttribute('class', 'icon');
+  $span.style.background = 'none';
+  $span.style.textIndent = '0';
+  $span.style.fontSize = '16px';
 
   var $a = document.createElement('a');
-  $a.appendChild($icon);
+  $a.appendChild($span);
   $a.setAttribute('href', href);
+  $a.setAttribute('class', 'sq_button tip');
   $a.dataset.tip = name;
 
   var $li = document.createElement('li');
